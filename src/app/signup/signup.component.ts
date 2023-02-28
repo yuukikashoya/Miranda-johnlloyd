2de99d22981c133bd62d4a7490743b3d18044a70
johnlloyd <yukimiranda26@gmail.com>
2023-03-01 01:22:55 +0800

    user account rewrite fix

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Database,set,ref } from '@angular/fire/database';
+import { Database,set,ref,onValue } from '@angular/fire/database';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,27 +16,39 @@ export class SignupComponent implements OnInit {
   }
 uuid = "";
   registerUser(value:any){
-    if(value.email == null || value.email == "" || value.password == null || value.password == "" 
-    ||  value.name == null || value.name == ""
-    
-    ){
-      alert('Fill the form ');
-    }
 
+    const starCountRef = ref(this.database, 'accounts/' + value.email);
+    onValue(starCountRef, (snapshot) => {
+     const db = snapshot.val();  
+  
+     if (db.email == value.email){
+      alert('user email already exist!');
+     }else{
+      if(value.email == null || value.email == "" || value.password == null || value.password == "" 
+      ||  value.name == null || value.name == ""
       
-    else {
-      this.uuid = "user" +Math.floor(100000 + Math.random() * 900000);
-  set(ref(this.database, 'accounts/' + value.email), {
-      id: this.uuid,
-      email: value.email,
-      name: value.name,
-      password: value.password
-
-
+      ){
+        alert('Fill the form ');
+      }
+  
+        
+      else {
+        this.uuid = "user" +Math.floor(100000 + Math.random() * 900000);
+    set(ref(this.database, 'accounts/' + value.email), {
+        id: this.uuid,
+        email: value.email,
+        name: value.name,
+        password: value.password
+  
+  
+       }); 
+       alert('account created!');
+       this.router.navigate(['/login'])
+      }
+     }
      }); 
-     alert('account created!');
-     this.router.navigate(['/login'])
-    }
+    
+
   }
 
 
