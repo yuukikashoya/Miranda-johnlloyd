b5fe2242f6a1fff56bcd07af41fb2a40f575aa31
johnlloyd <yukimiranda26@gmail.com>
2023-04-26 01:32:19 +0800

    notification added

--- src/app/navi/navi.component.ts
+++ src/app/navi/navi.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Database, onValue,ref } from '@angular/fire/database';
+import { Database, onValue,ref ,remove} from '@angular/fire/database';
 import { Router,NavigationStart, Event as NavigationEvent } from '@angular/router';
 
 @Component({
@@ -8,13 +8,14 @@ import { Router,NavigationStart, Event as NavigationEvent } from '@angular/route
   styleUrls: ['./navi.component.css']
 })
 export class NaviComponent implements OnInit {
+    name="";
   log =false;
   sign =false;
   dis = false;
   ala = false;
   event$ 
   val  ="" ;
-  name="";
+
   username = sessionStorage.getItem('id');
   constructor(private router:Router,public database: Database) {
 
@@ -27,17 +28,20 @@ export class NaviComponent implements OnInit {
               this.val = event.url;
 
               if( this.val== '/login'){
+       
                 this.log = true;
                 this.sign = false;
                 this.dis = false;
                 this.ala = false;
 
               }else if(this.val == '/signup'){
+         
                 this.sign = true;
                 this.log = false;
                 this.dis = false;
                 this.ala= false;
               }else if(this.val == '/display'){
+        
                 this.dis = true;
                 this.log= false;
                 this.sign = false;
@@ -47,20 +51,20 @@ export class NaviComponent implements OnInit {
                 this.log=false;
                 this.sign=false;
                 this.dis=false;
-          
+      
               }
             }
           });
 
 
-          const starCountRef = ref(this.database,'notify' + this.username);
-          onValue(starCountRef, (snapshot) => {
+          const starCountRef = ref(this.database,'notification/notify/' + this.username);
+           onValue(starCountRef, (snapshot) => {
             const db = snapshot.val();
-            this.name = db.name;
+            this.name = db.poster;
       
-          })
-              console.log(this.name)
-
+          });
+           
+ 
     
    }
 
@@ -70,6 +74,11 @@ export class NaviComponent implements OnInit {
   ngOnDestroy() {
     this.event$.unsubscribe();
   }
-
+  readed(){
+    remove(ref(this.database, 'notification/notify/' + this.username));
+    console.log(this.username);
+    this.name="";
+    this.router.navigate(['/noti'])
+  }
 
 }
